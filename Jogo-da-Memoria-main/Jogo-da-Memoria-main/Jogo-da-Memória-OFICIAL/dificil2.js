8c9fe0da0987b24d62defa433471d2fff7ee11f3
Gustavo Emanuel Abreu Ribeiro <126984553+Gustavo-Abreu-Ribeiro@users.noreply.github.com>
2024-11-20 20:45:57 -0300

    Add files via upload
    versão atualizando score e corrigindo alguns nomes

---
+++ Jogo-da-Memoria-main/Jogo-da-Memoria-main/Jogo-da-Memória-OFICIAL/dificil2.js
@@ -0,0 +1,138 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Opções de cartas
+  const cards = [
+    { name: "boi", img: "images/DIFICIL/BOI.png" },
+    { name: "boto", img: "images/DIFICIL/BOTO.png" },
+    { name: "caipora", img: "images/DIFICIL/CAIPORA.png" },
+    { name: "cuca", img: "images/DIFICIL/CUCA.png" },
+    { name: "curupira", img: "images/DIFICIL/CURUPIRA.png" },
+    { name: "iara", img: "images/DIFICIL/IARA.png" },
+    { name: "lobisomen", img: "images/DIFICIL/LOBISOMEN.png" },
+    { name: "mula", img: "images/DIFICIL/MULA.png" },
+    { name: "saci", img: "images/DIFICIL/SACI.png" },
+    { name: "boi", img: "images/DIFICIL/BOI.png" },
+    { name: "boto", img: "images/DIFICIL/BOTO.png" },
+    { name: "caipora", img: "images/DIFICIL/CAIPORA.png" },
+    { name: "cuca", img: "images/DIFICIL/CUCA.png" },
+    { name: "curupira", img: "images/DIFICIL/CURUPIRA.png" },
+    { name: "iara", img: "images/DIFICIL/IARA.png" },
+    { name: "lobisomen", img: "images/DIFICIL/LOBISOMEN.png" },
+    { name: "mula", img: "images/DIFICIL/MULA.png" },
+    { name: "saci", img: "images/DIFICIL/SACI.png" }
+  ];
+// Embaralhar cartas
+  cards.sort(() => 0.5 - Math.random());
+
+  // Elementos da tela
+  const board = document.querySelector(".board");
+  const resultView = document.querySelector("#result");
+
+  let cardsChosen = [];
+  let cardsChosenId = [];
+  let cardsWon = [];
+  let score = parseInt(localStorage.getItem('scoreDificil')) || 0; // Recuperar score específico do "Fácil"
+
+  // Função para mostrar o popup
+  function showPopup(message) {
+    const popup = document.createElement("div");
+    popup.className = "popup";
+    popup.innerText = message;
+
+    popup.style.position = "fixed";
+    popup.style.top = "10px";
+    popup.style.left = "50%";
+    popup.style.transform = "translateX(-50%)";
+    popup.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+    popup.style.color = "white";
+    popup.style.padding = "10px 20px";
+    popup.style.borderRadius = "5px";
+    popup.style.zIndex = "1000";
+
+    document.body.appendChild(popup);
+
+    // Remover o popup após 3 segundos
+    setTimeout(() => popup.remove(), 3000);
+  }
+
+  // Função para criar o tabuleiro
+  function createBoard() {
+    for (let i = 0; i < cards.length; i++) {
+      const card = document.createElement("img");
+      card.setAttribute("src", "images/DIFICIL/DIFICIL.png");
+      card.setAttribute("data-id", i);
+      card.addEventListener("click", flipCard);
+      board.appendChild(card);
+    }
+  }
+
+  // Checar combinação das cartas
+  function checkForMatch() {
+    const cards = document.querySelectorAll("img");
+    const optionOneId = cardsChosenId[0];
+    const optionTwoId = cardsChosenId[1];
+
+    // Verificar se clicou na mesma carta
+    if (optionOneId == optionTwoId) {
+      setTimeout(() => {
+        cards[optionOneId].setAttribute("src", "images/DIFICIL/DIFICIL.png");
+        cards[optionTwoId].setAttribute("src", "images/DIFICIL/DIFICIL.png");
+      }, 1000); // Demora 5 segundos para fechar
+      showPopup("Você clicou na mesma imagem");
+      score--; // Subtrair ponto por erro
+    }
+    // Verificar se as cartas combinam
+    else if (cardsChosen[0] === cardsChosen[1]) {
+      cards[optionOneId].setAttribute("src", "images/DIFICIL/CHECK_DIFICIL.png");
+      cards[optionTwoId].setAttribute("src", "images/DIFICIL/CHECK_DIFICIL.png");
+      cards[optionOneId].removeEventListener("click", flipCard);
+      cards[optionTwoId].removeEventListener("click", flipCard);
+      cardsWon.push(cardsChosen);
+      score++; // Adicionar ponto por par certo
+      showPopup("Você encontrou uma combinação");
+    } else {
+      setTimeout(() => {
+        cards[optionOneId].setAttribute("src", "images/DIFICIL/DIFICIL.png");
+        cards[optionTwoId].setAttribute("src", "images/DIFICIL/DIFICIL.png");
+      }, 1000); // Demora 5 segundos para fechar
+      showPopup("Errou, tente novamente");
+    }
+
+    cardsChosen = [];
+    cardsChosenId = [];
+    resultView.textContent = "Pares Encontrados: " + cardsWon.length;
+
+    // Salvar score específico do "Dificil" no localStorage
+    localStorage.setItem('scoreDificil', score);
+
+    // Atualizar pontuação na tela
+    updateScore();
+
+    // Verificar se o jogo acabou
+    if (cardsWon.length === cards.length / 2) {
+      resultView.textContent = "Parabéns! Você encontrou todas as cartas";
+    }
+  }
+
+  // Virar a carta
+  function flipCard() {
+    let cardId = this.getAttribute("data-id");
+    cardsChosen.push(cards[cardId].name);
+    cardsChosenId.push(cardId);
+    this.setAttribute("src", cards[cardId].img);
+
+    if (cardsChosen.length === 2) {
+      setTimeout(checkForMatch, 200); // Verifica a correspondência após 200ms
+    }
+  }
+
+  // Atualizar pontuação na página
+  function updateScore() {
+    const scoreElements = document.querySelectorAll(".score");
+    scoreElements.forEach(scoreElement => {
+      scoreElement.textContent = score;
+    });
+  }
+
+  // Criar o tabuleiro
+  createBoard();
+});
---
+++ Jogo-da-Memoria-main/Jogo-da-Memoria-main/Jogo-da-Memória-OFICIAL/media2.js
@@ -0,0 +1,134 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Opções de cartas
+  const cards = [
+    { name: "alvin", img: "images/MEDIA/ALVIN.png" },
+    { name: "bob", img: "images/MEDIA/BOB.png" },
+    { name: "jessie", img: "images/MEDIA/JESSIE.png" },
+    { name: "mario", img: "images/MEDIA/MARIO.png" },
+    { name: "monica", img: "images/MEDIA/MONICA.png" },
+    { name: "patrick", img: "images/MEDIA/PATRICK.png" },
+    { name: "alvin", img: "images/MEDIA/ALVIN.png" },
+    { name: "bob", img: "images/MEDIA/BOB.png" },
+    { name: "jessie", img: "images/MEDIA/JESSIE.png" },
+    { name: "mario", img: "images/MEDIA/MARIO.png" },
+    { name: "monica", img: "images/MEDIA/MONICA.png" },
+    { name: "patrick", img: "images/MEDIA/PATRICK.png" }
+  ];
+// Embaralhar cartas
+  cards.sort(() => 0.5 - Math.random());
+
+  // Elementos da tela
+  const board = document.querySelector(".board");
+  const resultView = document.querySelector("#result");
+
+  let cardsChosen = [];
+  let cardsChosenId = [];
+  let cardsWon = [];
+  let score = parseInt(localStorage.getItem('scoreMedio')) || 0; // Recuperar score específico do "Fácil"
+
+  // Função para mostrar o popup
+  function showPopup(message) {
+    const popup = document.createElement("div");
+    popup.className = "popup";
+    popup.innerText = message;
+
+    popup.style.position = "fixed";
+    popup.style.top = "10px";
+    popup.style.left = "50%";
+    popup.style.transform = "translateX(-50%)";
+    popup.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+    popup.style.color = "white";
+    popup.style.padding = "10px 20px";
+    popup.style.borderRadius = "5px";
+    popup.style.zIndex = "1000";
+
+    document.body.appendChild(popup);
+
+    // Remover o popup após 3 segundos
+    setTimeout(() => popup.remove(), 3000);
+  }
+
+  // Função para criar o tabuleiro
+  function createBoard() {
+    for (let i = 0; i < cards.length; i++) {
+      const card = document.createElement("img");
+      card.setAttribute("src", "images/MEDIA/MEDIA.png");
+      card.setAttribute("data-id", i);
+      card.addEventListener("click", flipCard);
+      board.appendChild(card);
+    }
+  }
+
+  // Checar combinação das cartas
+  function checkForMatch() {
+    const cards = document.querySelectorAll("img");
+    const optionOneId = cardsChosenId[0];
+    const optionTwoId = cardsChosenId[1];
+
+    // Verificar se clicou na mesma carta
+    if (optionOneId == optionTwoId) {
+      setTimeout(() => {
+        cards[optionOneId].setAttribute("src", "images/MEDIA/MEDIA.png");
+        cards[optionTwoId].setAttribute("src", "images/MEDIA/MEDIA.png");
+      }, 1000); // Demora 5 segundos para fechar
+      showPopup("Você clicou na mesma imagem");
+      score--; // Subtrair ponto por erro
+    }
+    // Verificar se as cartas combinam
+    else if (cardsChosen[0] === cardsChosen[1]) {
+      cards[optionOneId].setAttribute("src", "images/MEDIA/CHECK_MEDIA.png");
+      cards[optionTwoId].setAttribute("src", "images/MEDIA/CHECK_MEDIA.png");
+      cards[optionOneId].removeEventListener("click", flipCard);
+      cards[optionTwoId].removeEventListener("click", flipCard);
+      cardsWon.push(cardsChosen);
+      score++; // Adicionar ponto por par certo
+      showPopup("Você encontrou uma combinação");
+    } else {
+      setTimeout(() => {
+        cards[optionOneId].setAttribute("src", "images/MEDIA/MEDIA.png");
+        cards[optionTwoId].setAttribute("src", "images/MEDIA/MEDIA.png");
+      }, 1000); // Demora 5 segundos para fechar
+      showPopup("Errou, tente novamente");
+    }
+
+    cardsChosen = [];
+    cardsChosenId = [];
+    resultView.textContent = "Pares Encontrados: " + cardsWon.length;
+
+    // Salvar score específico do "Medio" no localStorage
+    localStorage.setItem('scoreMedio', score);
+
+    // Atualizar pontuação na tela
+    updateScore();
+
+    // Verificar se o jogo acabou
+    if (cardsWon.length === cards.length / 2) {
+      resultView.textContent = "Parabéns! Você encontrou todas as cartas";
+    }
+  }
+
+  // Virar a carta
+  function flipCard() {
+    let cardId = this.getAttribute("data-id");
+    cardsChosen.push(cards[cardId].name);
+    cardsChosenId.push(cardId);
+    this.setAttribute("src", cards[cardId].img);
+
+    if (cardsChosen.length === 2) {
+      setTimeout(checkForMatch, 200); // Verifica a correspondência após 200ms
+    }
+  }
+
+  // Atualizar pontuação na página
+  function updateScore() {
+    const scoreElements = document.querySelectorAll(".score");
+    scoreElements.forEach(scoreElement => {
+      scoreElement.textContent = score;
+    });
+  }
+
+  // Criar o tabuleiro
+  createBoard();
+});
+
+
